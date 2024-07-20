6e9e742447bf15770ac9d0b55ffa6d265782ffcd
evalshine <dankupr21@gmail.com>
2024-07-20 17:25:45 +0300

    asdawd

--- store/settings.py
+++ store/settings.py
@@ -54,6 +54,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '155751116358-ol9edr6nd1l0b2jv0hlr8gc9lt9hdrlt.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 
 SITE_ID = 1
 
