1fc0351735042677a9f0672c8abc48a35a990f1f
evalshine <dankupr21@gmail.com>
2024-07-30 23:25:45 +0300

    init

--- store/settings.py
+++ store/settings.py
@@ -64,6 +64,18 @@ SOCIAL_AUTH_PIPELINE = (
     '{BASE_DIR}.social_auth_pipeline.associate_by_email',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    '{BASE_DIR}.social_auth_pipeline.associate_by_email',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
