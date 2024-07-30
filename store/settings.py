3aef8a65519c14052bd945e2096857ad861e0846
evalshine <dankupr21@gmail.com>
2024-07-30 23:33:44 +0300

    init

--- store/settings.py
+++ store/settings.py
@@ -66,7 +66,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-    '{BASE_DIR}.social_auth_pipeline.associate_by_email',
+    'social_auth_pipeline.associate_by_email',  # Ваш пользовательский пайплайн
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.associate_by_email',
     'social_core.pipeline.social_auth.load_extra_data',
