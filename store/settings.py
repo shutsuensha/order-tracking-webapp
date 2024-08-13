23d17ff99013b0cf2dc6665461449f9b19173bac
evalshine <dankupr21@gmail.com>
2024-08-13 19:51:14 +0300

    init

--- store/settings.py
+++ store/settings.py
@@ -16,7 +16,7 @@ BOT_API=os.getenv('BOT_API')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = True
+DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -170,7 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
